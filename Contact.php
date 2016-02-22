ddf97378594854256323fae57787f2523476138e
Bishop Lafer <bishoplafer@Bishops-MacBook-Pro.local>
2016-02-21 17:00:30 -0800

    fishermensinn

---
+++ Contact.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact Fishermen's Inn</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="robots" content="index, follow" />
+    <meta name="msvalidate.01" content="4537DCA9585ADB7EB25DFE430D081EE3" />
+    <link rel="icon" type="favicon/png" href="images/favicon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <script src="email/validation.js" type="text/javascript"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+        <!-- Google Analytics -->
+  </head>
+  <body>
+    <header>
+      <nav id="navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="" href="index.html"><img src="images/navlogo.png" class="img-responsive pull-left" /></a>
+        </div>
+        <div class="collapse navbar-collapse" id="collapse">
+          <ul class="nav navbar-nav">
+            <li class=""><a href="index.html">Home</a></li>
+            <li class="dropdown">
+              <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Information <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="About.html">About Us</a></li>
+                <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="exclusiveaccess.html">Exclusive Fishing and Hiking</a></li>
+                <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="trips.html">Trip Packages</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="TheLodge.html">The Lodge</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="theskinny.html">The Skinny</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="tongass-national-park.html">Tongass National Forest</a></li>
+              </ul>
+            </li> <!-- End Dropdown -->
+            <li class="dropdown"role="presentation">
+                <a id="drop2" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Photos &amp; Videos <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu" aria-labelledby="drop2">
+                  <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="Alaska-the-Beautiful.html">Alaska The Beautiful</a></li>
+                  <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="Big-Fish-Wall.html">Big Fish Wall</a></li>
+                  <li class="" role="presentation"><a role="menuitem" tabindex="-1" href="Video-Gallery.html">Video Gallery</a></li>
+                </ul>
+            </li> <!-- end Photos & Videos -->
+            <li class="active" role="presentation"><a role="menuitem" tabindex="-1" href="Contact.html">Contact Us</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="thumbnail">
+            <a href="index.html" alt="The Fishermen's Inn"><img src="images/FishermensInn-logo.png" alt="The Fishermen's Inn - Port Alexander, Alaska" /></a>
+          </div><!-- /.thumbnail -->
+          <div class="hidden-sm hidden-xs thumbnail">
+            <h4>Let Us Get Your Gear!</h4>
+            <hr />
+            <p>Did you know we are Official Dealers for <a href="http://www.simmsfishing.com" alt="Simms Fishing Official Dealer">Simms</a> and <a href="http://www.sageflyfish.com" alt="Sage Fly Fishing Gear Official Dealer">Sage</a> Fishing gear? We can special order any gear you need and have it ready for you when you get here! Click the links below, find what you want, email or call us at 907-568-2399 with your request and we'll have it ready for you by the time you get here.</p>
+            <ul class="nav nav-pills nav-justified">
+              <li><a href="http://www.simmsfishing.com" alt="Simms Fishing Official Dealer">Simms</a></li>
+              <li><a href="http://www.sageflyfish.com" alt="Sage Fly Fishing Gear Official Dealer">Sage</a></li>
+            </ul>
+          </div><!-- /.hidden-sm .hidden-xs .thumbnail -->
+          <div class="hidden-sm hidden-xs thumbnail">
+            <br />
+            <a href="http://www.facebook.com/FishermensInn"><img src="images/facebook.jpg" alt="Fishermen's Inn on Facebook" class=
+            "img-responsive thumbnail" /></a>
+            <a href="http://www.tripadvisor.com/Hotel_Review-g31098-d1500031-Reviews-Fishermen_s_Inn-Port_Alexander_Alaska.html"><img src="images/trip-advisor.gif" alt="Check Us out on Trip Advisor" class=
+            "img-responsive thumbnail" /></a>
+          </div> <!-- end links section -->
+          <div class="hidden-sm hidden-xs thumbnail">
+            <h4>Friends</h4>
+            <hr />
+            <p>Chatham Strait Charters: Our friends run a professional charter outfit based just a few doors down the lane in Port Alexander, Alaska <a href="http://www.chathamstraitcharters.com" alt="Chatham Strait Charters">Chatham Strait Charters</a></p>
+          </div> <!-- end friends section -->
+        </div><!-- /.col-sm-3 -->
+        <div class="col-sm-9">
+          <h1>Contact Us</h1>
+          <ol class="breadcrumb">
+            <li><a href="index.html">Home</a></li>
+            <li class="active">Contact Us</li>
+          </ol>
+          <hr />
+          <!--Copy and paste the code below into your bootstrap html page -->
+          <div class="container-fluid">
+            <div class="col-sm-6">
+              <h3>Phone</h3>
+              <hr />
+              <p><strong>The Lodge</strong></p>
+              <p>907-568-2399</p>
+              <p><strong>Cell</strong></p>
+              <p>480-225-2217</p>
+            </div><!-- /.col-sm-6 -->
+            <div class="col-sm-6">
+              <h3>Mailing Address</h3>
+              <hr />
+              <p><strong>Fishermen’s Inn</strong></p>
+              <p>P.O. Box 8092</p>
+              <p>1 Breathless Lane</p>
+              <p>Port Alexander, AK 99836</p>
+            </div><!-- /.col-sm-6 -->
+            <div class="clearfix"></div><!-- /.clearfix -->
+            <h3>Email</h3>
+               <h3 style="color:#FF6633;"><?php echo $_GET[msg];?></h3>
+               <hr>
+                                                                
+            <form name="enq" method="post" action="email/" onsubmit="return validation();">
+            <fieldset>
+                
+            <input type="text" name="name" id="name" value=""  class="form-control" placeholder="Name" />
+            <br />
+            <input type="text" name="email" id="email" value="" class="form-control" placeholder="Email" />
+            <br />
+            <textarea rows="11" name="message" id="message" class="form-control" placeholder="Comments"></textarea>
+            <br />
+            <div class="actions">
+            <input type="submit" value="Send Your Message" name="submit" id="submitButton" class="btn btn-info pull-right" title="Click here to submit your message!" />
+            </div>
+              
+            </fieldset>
+            </form>
+          </div><!-- /.container-fluid -->
+        </div><!-- /.col-sm-9 --> 
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+                              <!-- Footer Row -->
+        <footer class="row" id="footer">
+            <div class="container">
+                <div class="col-xs-12">
+                    <ul class="list-inline small center-block">
+                      <li><a href="index.html" >Home</a></li>
+                      <li><a href="about.html" >About Us</a></li>
+                      <li><a href="exclusiveaccess.html" >Exclusive Access</a></li>
+                      <li><a href="theLodge.html">The Lodge</a></li>
+                      <li><a href="theskinny.html">The Skinny</a></li>
+                      <li><a href="tongass-national-park.html">Tongass National Forest</a></li>
+                      <li><a href="Alaska-the-Beautiful.html">Alaska the Beautiful</a></li>
+                      <li><a href="Big-Fish-Wall.html">Big Fish Wall</a></li>
+                      <li><a href="Video-Gallery.html">Video Gallery</a></li>
+                      <li><a href="Contact.html">Contact Us</a></li>
+                      <li><a href="site_map.html" >Site Map</a></li>
+                    </ul>
+                    <p class="small"> &copy; 2014 Fishermen's Inn. All rights reserved.</p>
+                </div><!-- /.col-xs-12 -->
+            </div><!-- /.container -->
+        </footer>
+    <script src="js/bootstrap-lightbox.min.js"></script>   
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
